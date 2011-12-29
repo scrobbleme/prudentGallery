f88d995e1452736b3cae3f9b21a82bc26e299357
Adrian Mörchen <adrian-git@rest.scrobble.me>
2011-12-29 11:09:23 +0100

    Separated default ImageScaler.

--- image-utils/src/main/java/me/scrobble/images/scalers/ImageScaler.java
+++ image-utils/src/main/java/me/scrobble/images/scalers/ImageScaler.java
@@ -2,21 +2,23 @@ package me.scrobble.images.scalers;
 
 /**
  * Interface for an image scaler
+ * 
+ * @author Adrian M&ouml;rchen - <a href="http://www.scrobble.me"
+ *         target="_blank">http://www.scrobble.me</a>
  */
 public interface ImageScaler {
-	/**
-	 * @param sourcePath
-	 *            Path of the source image.
-	 * @param targetPath
-	 *            Path to the target image.
-	 * @param width
-	 *            The final width.
-	 * @param height
-	 *            The final height.
-	 * @param preserveAspectRation
-	 *            <code>true</code> if the aspect ration should be reserved.
-	 */
-	public void scale(String sourcePath, String targetPath, int width,
-			int height, boolean preserveAspectRation)
-			throws ImageScalerException;
+    /**
+     * @param sourcePath
+     *            Path of the source image.
+     * @param targetPath
+     *            Path to the target image.
+     * @param width
+     *            The final width.
+     * @param height
+     *            The final height.
+     * @param preserveAspectRatio
+     *            <code>true</code> if the aspect ratio should be reserved.
+     */
+    public void scale(String sourcePath, String targetPath, int width, int height, boolean preserveAspectRatio)
+            throws ImageScalerException;
 }
