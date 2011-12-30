5107a9dc7bc70d6b6a6d327ade23db2cb1f3ecdc
Adrian Mörchen <github.com@rest.scrobble.me>
2011-12-30 14:02:17 +0100

    [Issue-2] Implement ImageScaler for Linux: Added config example for ImageMagick.

--- prudentGallery/src/main/resources/settings.js
+++ prudentGallery/src/main/resources/settings.js
@@ -9,5 +9,11 @@ applicationContactEmail = 'prudentGallery@rest.scrobble.me'
 
 showDebugOnError = true
 
-// Enable IrfanView
-// Packages.me.scrobble.images.ImageUtils.setImageScaler(new Packages.me.scrobble.images.scalers.IrfanViewImageScaler())
+// IrfanView (Windows only)
+//Packages.me.scrobble.images.ImageUtils.setImageScaler(new Packages.me.scrobble.images.scalers.IrfanViewImageScaler())
+
+// ImageMagick (Windows version, Linux commands might differ)
+//imagemagick_commands_thumbnail = 'cmd /C convert "@SOURCE@" -resize @WIDTH@x@HEIGHT@^^ -gravity center -extent @WIDTH@x@HEIGHT@ "@TARGET@"'
+//imagemagick_commands_large = 'cmd /C convert "@SOURCE@" -resize @WIDTH@x@HEIGHT@ "@TARGET@"'
+//Packages.me.scrobble.images.ImageUtils.setImageScaler(new Packages.me.scrobble.images.scalers.CommandLineImageScaler(imagemagick_commands_thumbnail,imagemagick_commands_large))
+
